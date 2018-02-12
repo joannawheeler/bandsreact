5b3e41ce523313c8e40e903cf58267ea991ced09
Joanna Wheeler <joannachristinewheeler@gmail.com>
2018-02-12 15:43:26 -0800

    Extracted URL parameters to display specified artist and artist's events

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Moment from 'react-moment';
-
+import qs  from 'qs';
 
 class ArtistHeader extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      artists: []
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://rest.bandsintown.com/artists/Maroon%205?app_id=bit_challenge")
-      .then(res => res.json())
-      .then(data =>
-      this.setState({
-        artist: data
-      }));
-  }
-
-  // test
 
   render() {
-    const { artist } = this.state;
+    const artist = this.props.artist;
+
     return (
       <div id="artistHeader">
         <div id="artistImage">
-          <img id="thumbImage" src={artist.thumb_url} />
+          <img id="thumbImage" src={artist.thumb_url} alt="" />
         </div>
         <div id="textRightOfImage">
           <strong id="artistName">{artist.name}</strong>
@@ -43,10 +28,10 @@ class EventList extends Component {
     const rows = [];
     const events = this.props.events;
 
-    {events.map(event =>
+    events.map(event =>
       rows.push(
         <EventRow event={event} key={event.id} />
-    ))};
+    ));
 
     return (
       <div>
@@ -61,50 +46,87 @@ class EventList extends Component {
 class EventRow extends Component {
   render() {
     const event = this.props.event;
-
     const location = event.venue.country === "United States" ? event.venue.city + ", " + event.venue.region : event.venue.city + ", " + event.venue.country;
-
-    const datetime = event.datetime
+    const datetime = event.datetime;
 
     return (
       <tr>
-        <td><Moment format="MMM DD">{datetime}</Moment></td>
-        <td>{event.venue.name}</td>
-        <td>{location}</td>
-        <td>tickets button</td>
+        <td id="monthDate"><Moment format="MMM DD">{datetime}</Moment></td>
+        <td id="venueName">{event.venue.name}</td>
+        <td id="venueLocation">{location}</td>
+        <td id="ticketsButtonCell"><a href="" id="ticketsButton">tickets button</a></td>
       </tr>
     );
   }
 }
 
-class App extends Component {
+class Container extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      events: []
-    };
+      events: [],
+      artist: [],
+      artistName: "",
+      auth: "?app_id=bit_challenge"
+    }
   }
 
   componentDidMount() {
-    fetch("https://rest.bandsintown.com/artists/Maroon%205/events?app_id=bit_challenge")
+    const params = window.location.search;
+    const parsedParam = qs.parse(params, { ignoreQueryPrefix: true });
+    const artistName = encodeURIComponent(parsedParam.artist.slice(1));
+    this.setState({
+      artistName: artistName
+    })
+    // const auth = "?app_id=bit_challenge";
+    //const requestURL = "https://rest.bandsintown.com/artists/" + artistName;
+    const requestURL = 'https://rest.bandsintown.com/artists/' + artistName;
+
+    fetch('https://rest.bandsintown.com/artists/' + artistName + this.state.auth)
       .then(res => res.json())
       .then(data =>
       this.setState({
-        events: data
+        artist: data,
+      }));
+    fetch(requestURL + '/events' + this.state.auth)
+      .then(res => res.json())
+      .then(data =>
+      this.setState({
+        events: data,
       }));
   }
+        // <Router>
+        // <div>
+          // <Route path={path}
 
   render() {
     const { events } = this.state;
+    const { artist } = this.state;
+    return (
+      <div>
+        <ArtistHeader artist={ artist } />
+        <EventList events={ events } />
+      </div>
+    )
+  }
+}
 
+class App extends Component {
+
+  render() {
     return (
       <div id="container">
-        <ArtistHeader />
-        <EventList events={this.state.events} />
+        <Router>
+        <div>
+            <Route path='/' component={ Container } />
+          </div>
+        </Router>
       </div>
     );
   }
 }
+//localhost:3000/?artist=:url_escaped_artist_name
+
 
 
 export default App;
